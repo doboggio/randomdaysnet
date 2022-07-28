0f38fb5a395d9d4d05742f2962448e8d9118e726
Dominic Boggio <dominicboggio@gmail.com>
2022-07-28 01:52:49 -0400

    Add website

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import { Link } from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://c.tenor.com/yE93MoMrKsUAAAAC/moai-emoji-moai.gif" className="App-logo" alt="logo" />
+        <p>
+          Welcome to my website!!!1!
+        </p>
+        <div className="NavButtons">
+            <a href=""></a>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
