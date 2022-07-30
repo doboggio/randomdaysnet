8974c7cfcddefae3295921025d3f65dd47242c7d
Dominic Boggio <dominicboggio@gmail.com>
2022-07-29 21:08:57 -0400

    Ok let's try this for real this time

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Header from "./Components/Header"
+
+class App extends React.Component{
+    render(){
+       return <div className="globalApp">
+        <Header />
+       </div>}
+}
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,29 +1,8 @@
-import ReactDOM from "react-dom/client";
-import './index.css'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./pages/Layout";
-import Home from "./pages/Home";
-import Contact from "./pages/Contact";
-import NoPage from "./pages/NoPage";
 import React from "react";
-
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="*" element={<NoPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
+import ReactDOM from 'react-dom/client';
+import './index.css'
+import App from './App.js'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+root.render(<App />);
